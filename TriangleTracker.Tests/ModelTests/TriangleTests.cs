4e9be21b122b030fcef776c66c110bee45442dfd
Noah Cowan <nsncowan@gmail.com>
2023-01-30 20:15:38 -0800

    add testing and branching logic
    Co-authored by: Geoffrey Gao <geoffreygao1@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,11 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using TriangleTracker;
 
-namespace ProjectName.Tests
+namespace TriangleTracker.Tests
 {
   [TestClass]
-  public class ClassNameTests
+  public class TriangleTests
   {
-    // Test methods go here
+//     [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    [TestMethod]
+    public void Triangle_CreateTriangle_True()
+    {
+      Triangle myTriangle = new Triangle(1,2,3);
+      Assert.AreEqual(1, myTriangle.Side1);
+      Assert.AreEqual(2, myTriangle.Side2);
+      Assert.AreEqual(3, myTriangle.Side3);
+    }
+
+    [TestMethod]
+    public void IsTriangle_CheckIfTriangle_true()
+    {
+      Triangle myTriangle = new Triangle(3,3,3);
+      Assert.AreEqual(true, myTriangle.IsTriangle());
+    }
+
+    [TestMethod]
+    public void TriangleType_ReturnTriangleType_string()
+    {
+      Triangle equilateral = new Triangle(3,3,3);
+      Assert.AreEqual("equilateral", equilateral.TriangleType());
+      Triangle isosceles = new Triangle(3,3,2);
+      Assert.AreEqual("isosceles", isosceles.TriangleType());
+      Triangle scalene = new Triangle(5,4,3);
+      Assert.AreEqual("scalene", scalene.TriangleType());
+      Triangle notTriangle = new Triangle(5,4,30);
+      Assert.AreEqual("not a triangle", notTriangle.TriangleType());
+    }
+
+
+
   }
 }
